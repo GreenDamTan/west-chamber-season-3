951a6764b21860c91236088de590abf34468bad3
System Administrator <root@localhost.(none)>
2012-04-12 01:06:57 +0800

    update ip block script

--- west-chamber-proxy/gen.sh
+++ west-chamber-proxy/gen.sh
@@ -1,9 +1,17 @@
 ip="74.125.71.0"
 ip_prefix="74.125.71"
 
-nmap -sS -p80 "$ip"/24 --host-timeout 5s --log-errors > .nmap5s.log
+nmap -sS -p80 "$ip"/24 --host-timeout 16s --log-errors > .nmap16s.log
+ssh liruqi@asuwish.cc 'sudo nmap -sS -p80 74.125.71.0/24 --host-timeout 16s --log-errors' > .nmap16s.remote.log
 
 cat .nmap16s.log | grep "$ip_prefix" | awk -v ip_prefix="$ip_prefix" '{
     if (index($5, ip_prefix) > 0) print $5;
     else print substr($6, 2, length($6)-2);
-}'
+}' > .availiplist.log
+
+cat .nmap16s.remote.log | grep "$ip_prefix" | awk -v ip_prefix="$ip_prefix" '{
+    if (index($4, ip_prefix) > 0) print substr($4, 1, length($4)-1);
+    else print substr($5, 2, length($5)-3);
+}' > .alliplist.log
+
+diff .availiplist.log .alliplist.log | grep "^>" | awk '{print $2}'
