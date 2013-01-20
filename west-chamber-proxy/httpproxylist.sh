1b29afbbe3aa8ae0db25c6516bdc82347bb351c9
liruqi <liruqi@gmail.com>
2013-01-20 11:51:22 +0800

    use bash instead of sh

--- west-chamber-proxy/httpproxylist.sh
+++ west-chamber-proxy/httpproxylist.sh
@@ -4,7 +4,6 @@ curl -iv http://www.ip-adress.com/proxy_list/ -H 'User-Agent:Mozilla/6.0 (Macint
 
 #seq command is outdated, this version is recommended, ref: http://www.cyberciti.biz/faq/bash-for-loop/
 for i in {1..10}; do
-    echo i
     curl -iv "http://www.cnproxy.com/proxy$i.html" -H 'User-Agent:Mozilla/6.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11' | awk -F'>|<' '/<td>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $5 ":" substr($7,19)}' | sed 's/+r/8/g' | sed 's/+d/0/g' | sed 's/+z/3/g' | sed 's/+m/4/g' | sed 's/+k/2/g' | sed 's/+l/9/g' | sed 's/+b/5/g' | sed 's/+i/7/g' | sed 's/+w/6/g' |sed 's/+c/1/g' |awk -F ')' '{print $1}' >> httpproxy.list
 done
 
