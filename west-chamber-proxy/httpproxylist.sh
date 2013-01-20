0ec630da41d321b839ed9e50800803500a5397eb
liruqi <liruqi@gmail.com>
2013-01-20 15:40:47 +0800

    fix

--- west-chamber-proxy/httpproxylist.sh
+++ west-chamber-proxy/httpproxylist.sh
@@ -11,10 +11,10 @@ for i in {1..10}; do
     curl -iv "http://www.cnproxy.com/proxy$i.html" -H 'User-Agent:Mozilla/6.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11' | awk -F'>|<' '/<td>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $5 ":" substr($7,19)}' | sed 's/+r/8/g' | sed 's/+d/0/g' | sed 's/+z/3/g' | sed 's/+m/4/g' | sed 's/+k/2/g' | sed 's/+l/9/g' | sed 's/+b/5/g' | sed 's/+i/7/g' | sed 's/+w/6/g' |sed 's/+c/1/g' |awk -F ')' '{print $1}' >> $WORK_DIR/www.cnproxy.com.list
 done
 
-curl http://www.proxynova.com/proxy_list.txt --max-time 6 | awk  '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ {print $1}' >> $WORK_DIR/www.proxynova.com.list
-curl http://www.searchlores.org/pxylist2.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' >> $WORK_DIR/www.searchlores.org.list
-curl http://www.freeproxy.ru/download/lists/goodproxy.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $2}' >> $WORK_DIR/www.freeproxy.ru.list
-curl http://www.binary-zone.com/files/MyProxyList.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1$2}' >> $WORK_DIR/www.binary-zone.com.list
+curl http://www.proxynova.com/proxy_list.txt --max-time 6 | awk  '/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ {print $1}' > $WORK_DIR/www.proxynova.com.list
+curl http://www.searchlores.org/pxylist2.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1}' > $WORK_DIR/www.searchlores.org.list
+curl http://www.freeproxy.ru/download/lists/goodproxy.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $2}' > $WORK_DIR/www.freeproxy.ru.list
+curl http://www.binary-zone.com/files/MyProxyList.txt | awk  '/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/ {print $1$2}' > $WORK_DIR/www.binary-zone.com.list
 
 for domain in "www.ip-adress.com" "www.cnproxy.com" "www.proxynova.com" "www.searchlores.org" "www.freeproxy.ru" "www.binary-zone.com"
 
